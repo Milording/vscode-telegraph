c6e9311e67f86a96e829469af5ab18b667ec088c
Anton Mitrofanov <milording@gmail.com>
2018-08-08 00:17:33 +0300

    Renaming of extension to Telepost

--- src/extension.ts
+++ src/extension.ts
@@ -5,30 +5,34 @@ const open = require('opn');
 const Telegraph = require('telegra.ph');
 
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.telepost', () => {
+        openTelegraph();
+    });
 
-    console.log('Congratulations, your extension "telegraph-poster" is now active!');
-    
+    context.subscriptions.push(disposable);
+}
+
+function openTelegraph() {
     const client = new Telegraph();
 
+    let editor = vscode.window.activeTextEditor as vscode.TextEditor;
+    var textOfFile = editor.document.getText();
+    
     let shortName = "Васек";
     let name = "Василий";
     let url = "http://google.com";
     client.createAccount(shortName, name, url).then((account:any)=> {
         client.token = account.access_token;
         let title = "Название";
-        let content = [new Node("p", ["Hello"])];
+        let content = [new Node("p", [textOfFile])];
         let authorName = "Создатель";
         let authorUrl = "http://google.com";
         let returnContent = false;
 
         return client.createPage(title, content, authorName, authorUrl, returnContent);
-    }).then((page:any) => open(page["url"]));
-
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        vscode.window.showInformationMessage('Hello World!');
+    }).then(async (page:any) => {  
+        open(page.url);
     });
-
-    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {
